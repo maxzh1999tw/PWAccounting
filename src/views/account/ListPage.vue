5f1486c141f3c27f33ee4e3438e66de2fe35909e
Max Zheng <34936931+maxzh1999tw@users.noreply.github.com>
2025-03-02 09:16:12 +0800

    cicd bug test

--- src/views/account/ListPage.vue
+++ src/views/account/ListPage.vue
@@ -1,4 +1,4 @@
-<script setup async lang="ts">
+<script setup lang="ts">
 import { computed, onMounted, onUnmounted, ref } from 'vue';
 
 import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
